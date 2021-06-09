bacb058d7358b1793f7d863cda658b43f5aa1bd8
KenFront <flamma0917@gmail.com>
2021-06-09 15:25:32 +0800

    Get todo list

--- db/init/1-init.sql
+++ db/init/1-init.sql
@@ -2,7 +2,7 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TYPE todo_status AS ENUM ('idle', 'completed');
 
-CREATE TABLE IF NOT EXISTS todolist (
+CREATE TABLE IF NOT EXISTS todos (
     id uuid DEFAULT uuid_generate_v4 (),
     title varchar(100) NOT NULL,
     description varchar(65535) NOT NULL,
--- src/main.go
+++ src/main.go
@@ -1,13 +1,52 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type Todo struct {
+	ID          string `gorm:"primaryKey"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Status      string `json:"status"`
+}
+
+func getEnv() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
 
 func main() {
+	getEnv()
+
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Hello World!!",
-		})
+		dsn := fmt.Sprintf("host=localhost user=%s password=%s dbname=%s port=%s sslmode=disable", os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_DB"), os.Getenv(("POSTGRES_PORT_HOST")))
+
+		db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+		if err == nil {
+			var todos []Todo
+
+			db.Find(&todos)
+
+			c.JSON(200, gin.H{
+				"data": todos,
+			})
+		} else {
+			c.JSON(200, gin.H{
+				"error": err.Error(),
+			})
+		}
 	})
 	r.Run()
 }
