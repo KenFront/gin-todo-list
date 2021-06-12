268fc9af102f273df827308d9baf515a3d1fd81b
KenFront <flamma0917@gmail.com>
2021-06-13 01:50:10 +0800

    Check if user is in db

--- src/middleware/authGuard.go
+++ src/middleware/authGuard.go
@@ -3,17 +3,39 @@ package middleware
 import (
 	"net/http"
 
+	"github.com/KenFront/gin-todo-list/src/config"
+	"github.com/KenFront/gin-todo-list/src/model"
 	"github.com/KenFront/gin-todo-list/src/util"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
 
 func authGuard() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		_, err := util.CheckAuth(c)
+		claim, err := util.CheckAuth(c)
 		if err != nil {
 			util.DeleteAuth(c)
 			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
 				"error": err.Error(),
+			})
+		}
+
+		id, err := uuid.Parse(claim.UserId)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"error": err.Error(),
+			})
+		}
+
+		user := model.User{
+			ID: id,
+		}
+
+		result := config.GetDB().First(&user, "id = ?", id)
+
+		if result.Error != nil {
+			c.AbortWithStatusJSON(http.StatusServiceUnavailable, gin.H{
+				"error": result.Error.Error(),
 			})
 		}
 		c.Next()
