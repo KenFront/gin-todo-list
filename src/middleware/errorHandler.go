79854e8965db1bd24944a2e36c469118992eaf5b
KenFront <flamma0917@gmail.com>
2021-06-13 18:57:18 +0800

    Refactor error handler

--- src/middleware/errorHandler.go
+++ src/middleware/errorHandler.go
@@ -7,20 +7,22 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func catchError(c *gin.Context) {
+	r := recover()
+	switch {
+	case util.IsSameType(r, &util.ApiError{}):
+		err := r.(*util.ApiError)
+		c.AbortWithStatusJSON(err.StatusCode, gin.H{
+			"error": err.ErrorType,
+		})
+	case r != nil:
+		c.AbortWithStatus(http.StatusInternalServerError)
+	}
+}
+
 func errorHandler() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		defer func() {
-			r := recover()
-			switch {
-			case util.IsSameType(r, &util.ApiError{}):
-				err := r.(*util.ApiError)
-				c.AbortWithStatusJSON(err.StatusCode, gin.H{
-					"error": err.ErrorType,
-				})
-			case r != nil:
-				c.AbortWithStatus(http.StatusInternalServerError)
-			}
-		}()
+		defer catchError(c)
 		c.Next()
 	}
 }
