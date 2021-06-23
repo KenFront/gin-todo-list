a9a1d191564366b69efb585807d04778089e29e0
KenFront <flamma0917@gmail.com>
2021-06-23 21:59:17 +0800

    Add test case for patch user

---
+++ src/controller/users/patch_test.go
@@ -0,0 +1,148 @@
+package controller_users_test
+
+import (
+	"net/http"
+	"testing"
+
+	controller_users "github.com/KenFront/gin-todo-list/src/controller/users"
+	"github.com/KenFront/gin-todo-list/src/mock"
+	"github.com/KenFront/gin-todo-list/src/model"
+	"github.com/KenFront/gin-todo-list/src/util"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPatchUserSuccess(t *testing.T) {
+	resForAdd := mock.GetResponse()
+	userId := util.GetNewTodoId()
+	cRorAdd := mock.GetGinContext(resForAdd)
+
+	gormDB := mock.GetMockGorm(t)
+
+	fake := mock.GetMockUser()
+
+	cRorAdd.Request = &http.Request{
+		Header: make(http.Header),
+		Body:   mock.GetRequsetBody(fake),
+	}
+
+	controller_users.Add(controller_users.AddProps{
+		Db:           gormDB,
+		GetNewUserId: func() uuid.UUID { return userId },
+	})(cRorAdd)
+
+	assert.Equal(t, http.StatusOK, resForAdd.Code)
+
+	resForPatch := mock.GetResponse()
+	cRorPatch := mock.GetGinContext(resForPatch)
+	cRorPatch.Set("userId", userId)
+
+	payload := model.PatchUser{
+		Name: "123",
+	}
+
+	cRorPatch.Request = &http.Request{
+		Header: make(http.Header),
+		Body:   mock.GetRequsetBody(payload),
+	}
+
+	controller_users.Patch(controller_users.PatchProps{
+		Db: gormDB,
+	})(cRorPatch)
+
+	var resBody SuccessUserAPIResponse
+	mock.GetResponseBody(resForPatch.Body.Bytes(), &resBody)
+
+	assert.Equal(t, http.StatusOK, resForPatch.Code)
+	assert.Equal(t, payload.Name, resBody.Data.Name)
+	assert.Equal(t, fake.Email, resBody.Data.Email)
+	assert.Equal(t, fake.Account, resBody.Data.Account)
+}
+
+func TestPatchUserFailedByNotSignIn(t *testing.T) {
+	res := mock.GetResponse()
+	c := mock.GetGinContext(res)
+
+	payload := model.PatchUser{
+		Name: "123",
+	}
+
+	c.Request = &http.Request{
+		Header: make(http.Header),
+		Body:   mock.GetRequsetBody(payload),
+	}
+
+	gormDB := mock.GetMockGorm(t)
+
+	defer func() {
+		r := recover()
+		if r == nil {
+			t.Errorf("The code did not panic")
+		}
+		err := r.(*model.ApiError)
+		assert.Equal(t, http.StatusBadRequest, err.StatusCode)
+		assert.Equal(t, model.ERROR_SIGN_IN_FAILED, err.ErrorType)
+	}()
+
+	controller_users.Patch(controller_users.PatchProps{
+		Db: gormDB,
+	})(c)
+}
+
+func TestPatchUserFailedByNoNeededPayload(t *testing.T) {
+	res := mock.GetResponse()
+	c := mock.GetGinContext(res)
+
+	payload := model.PatchUser{}
+
+	c.Request = &http.Request{
+		Header: make(http.Header),
+		Body:   mock.GetRequsetBody(payload),
+	}
+
+	gormDB := mock.GetMockGorm(t)
+
+	defer func() {
+		r := recover()
+		if r == nil {
+			t.Errorf("The code did not panic")
+		}
+		err := r.(*model.ApiError)
+		assert.Equal(t, http.StatusBadRequest, err.StatusCode)
+		assert.Equal(t, model.ERROR_NO_VALUE_IN_PATCH_USER_PAYLOAD, err.ErrorType)
+	}()
+
+	controller_users.Patch(controller_users.PatchProps{
+		Db: gormDB,
+	})(c)
+}
+
+func TestPatchUserFailedByNoUserId(t *testing.T) {
+	res := mock.GetResponse()
+	c := mock.GetGinContext(res)
+
+	payload := model.PatchUser{
+		Name: "123",
+	}
+
+	c.Request = &http.Request{
+		Header: make(http.Header),
+		Body:   mock.GetRequsetBody(payload),
+	}
+
+	gormDB := mock.GetMockGorm(t)
+
+	defer func() {
+		r := recover()
+		if r == nil {
+			t.Errorf("The code did not panic")
+		}
+		err := r.(*model.ApiError)
+		assert.Equal(t, http.StatusBadRequest, err.StatusCode)
+		assert.Equal(t, model.ERROR_SIGN_IN_FAILED, err.ErrorType)
+	}()
+
+	controller_users.Patch(controller_users.PatchProps{
+		Db: gormDB,
+	})(c)
+}
