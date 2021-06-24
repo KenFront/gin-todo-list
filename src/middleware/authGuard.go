bc502d6b41182d8dfc6f771ea43fed5745fa16b5
KenFront <flamma0917@gmail.com>
2021-06-24 22:33:35 +0800

    Add authGuard test

--- src/middleware/authGuard.go
+++ src/middleware/authGuard.go
@@ -8,37 +8,46 @@ import (
 	"github.com/KenFront/gin-todo-list/src/model"
 	"github.com/KenFront/gin-todo-list/src/util"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"gorm.io/gorm"
 )
 
-func authGuard(c *gin.Context) {
-	id, err := util.GetUserIdByToken(c)
-	if err != nil {
-		util.DeleteAuth(c)
-		controller.ApiOnError(&model.ApiError{
-			StatusCode: http.StatusBadRequest,
-			ErrorType:  model.ErrorType(err.Error()),
-			Error:      err,
-		})
+type AuthGuardProps struct {
+	Db               *gorm.DB
+	GetUserIdByToken func(c *gin.Context) (uuid.UUID, error)
+}
+
+func AuthGuard(p AuthGuardProps) func(c *gin.Context) {
+	return func(c *gin.Context) {
+		id, err := p.GetUserIdByToken(c)
+		if err != nil {
+			util.DeleteAuth(c)
+			controller.ApiOnError(&model.ApiError{
+				StatusCode: http.StatusBadRequest,
+				ErrorType:  model.ErrorType(err.Error()),
+				Error:      err,
+			})
+		}
+
+		if err := p.Db.First(&model.User{}, "id = ?", id).Error; err != nil {
+			util.DeleteAuth(c)
+			controller.ApiOnError(&model.ApiError{
+				StatusCode: http.StatusServiceUnavailable,
+				ErrorType:  model.ERROR_SIGN_IN_FAILED,
+				Error:      err,
+			})
+		}
+
+		controller.SetUserId(c, id)
+
+		c.Next()
 	}
-
-	user := model.User{
-		ID: id,
-	}
-
-	if err := config.GetDB().First(&user, "id = ?", id).Error; err != nil {
-		util.DeleteAuth(c)
-		controller.ApiOnError(&model.ApiError{
-			StatusCode: http.StatusServiceUnavailable,
-			ErrorType:  model.ERROR_SIGN_IN_FAILED,
-			Error:      err,
-		})
-	}
-
-	controller.SetUserId(c, id)
-
-	c.Next()
 }
 
 func UseAuthGuard(r *gin.RouterGroup) gin.IRoutes {
-	return r.Use(authGuard)
+	db := config.GetDB()
+	return r.Use(AuthGuard(AuthGuardProps{
+		Db:               db,
+		GetUserIdByToken: util.GetUserIdByToken,
+	}))
 }
---
+++ src/middleware/authGuard_test.go
@@ -0,0 +1,93 @@
+package middleware_test
+
+import (
+	"errors"
+	"net/http"
+	"testing"
+
+	"github.com/KenFront/gin-todo-list/src/controller"
+	controller_users "github.com/KenFront/gin-todo-list/src/controller/users"
+	"github.com/KenFront/gin-todo-list/src/model"
+	"github.com/gin-gonic/gin"
+
+	"github.com/KenFront/gin-todo-list/src/middleware"
+	"github.com/KenFront/gin-todo-list/src/mock"
+	"github.com/KenFront/gin-todo-list/src/util"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAuthGuardSuccess(t *testing.T) {
+	res := mock.GetResponse()
+	userId := util.GetNewTodoId()
+	c := mock.GetGinContext(res)
+
+	gormDB := mock.GetMockGorm(t)
+
+	fake := mock.GetMockUser()
+
+	c.Request = &http.Request{
+		Header: make(http.Header),
+		Body:   mock.GetRequsetBody(fake),
+	}
+
+	controller_users.Add(controller_users.AddProps{
+		Db:           gormDB,
+		GetNewUserId: func() uuid.UUID { return userId },
+	})(c)
+
+	assert.Equal(t, http.StatusOK, res.Code)
+
+	middleware.AuthGuard(middleware.AuthGuardProps{
+		Db:               gormDB,
+		GetUserIdByToken: mock.UtilGetUserIdByToken(userId),
+	})(c)
+
+	assert.Equal(t, userId, controller.GetUserId(c))
+}
+
+func TestAuthGuardFailByParseUserIdFailed(t *testing.T) {
+	res := mock.GetResponse()
+	c := mock.GetGinContext(res)
+	customError := ""
+
+	gormDB := mock.GetMockGorm(t)
+
+	defer func() {
+		r := recover()
+		if r == nil {
+			t.Errorf("The code did not panic")
+		}
+		err := r.(*model.ApiError)
+		assert.Equal(t, http.StatusBadRequest, err.StatusCode)
+		assert.Equal(t, model.ErrorType(customError), err.ErrorType)
+	}()
+
+	middleware.AuthGuard(middleware.AuthGuardProps{
+		Db:               gormDB,
+		GetUserIdByToken: func(c *gin.Context) (uuid.UUID, error) { return uuid.Nil, errors.New(customError) },
+	})(c)
+}
+
+func TestAuthGuardFailByUserNotExist(t *testing.T) {
+	res := mock.GetResponse()
+	userId := util.GetNewTodoId()
+	c := mock.GetGinContext(res)
+
+	gormDB := mock.GetMockGorm(t)
+
+	defer func() {
+		r := recover()
+		if r == nil {
+			t.Errorf("The code did not panic")
+		}
+		err := r.(*model.ApiError)
+		assert.Equal(t, http.StatusServiceUnavailable, err.StatusCode)
+		assert.Equal(t, model.ERROR_SIGN_IN_FAILED, err.ErrorType)
+	}()
+
+	middleware.AuthGuard(middleware.AuthGuardProps{
+		Db:               gormDB,
+		GetUserIdByToken: mock.UtilGetUserIdByToken(userId),
+	})(c)
+}
