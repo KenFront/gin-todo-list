e1805bbee4a2c8bfe17fda6d8c8ec4eb7bba1194
KenFront <flamma0917@gmail.com>
2021-06-26 19:58:30 +0800

    Fix wrong unique sql syntex

--- db/migrations/4_soft-delete-use.up.sql
+++ db/migrations/4_soft-delete-use.up.sql
@@ -1,4 +1,3 @@
 ALTER TABLE users ADD COLUMN deleted_at timestamp;
-ALTER TABLE users
-    DROP CONSTRAINT IF EXISTS users_account_key,
-    ADD UNIQUE (account, deleted_at);
+ALTER TABLE users DROP CONSTRAINT IF EXISTS users_account_key;
+CREATE UNIQUE INDEX users_account_deleted_at_key ON users(account) WHERE deleted_at IS NULL;
